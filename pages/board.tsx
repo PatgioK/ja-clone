e83b80c99ec68289b31bd761921af9219e47fec1
pat <patgiok@gmail.com>
2022-09-26 19:45:19 -0700

    add apolloclient pkg + starting ui board

---
+++ pages/board.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+function board() {
+  return (
+    <div>board</div>
+  )
+}
+
+export default board
