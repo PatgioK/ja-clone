025600d8a382366e3ee4468516cde264ae27aa34
pat <patgiok@gmail.com>
2022-09-27 04:41:05 -0700

    install rest fontawesome, boardSection added

--- components/boardSection.tsx
+++ components/boardSection.tsx
@@ -1,20 +1,37 @@
 
 import React from 'react'
 import { TaskComponent } from './taskComponent'
+
 import { Button, Col, Card, Form, Container } from 'react-bootstrap'
+import tasks from '../pages/api/tasks'
 
 interface BoardSectionProps {
     title: string
     tasks?: Task[]
 }
 
-export const boardSection = (props: BoardSectionProps) => {
+export const BoardSection = (props: BoardSectionProps) => {
     return (
         <>
             <Col md={3} className='d-flex flex-column p-2'>
                 <div className="board-section-header d-flex flex-row align-items-center">
                     <h3>{props.title}</h3>
+                    {/* <<FontAwesomeIcon icon="fa-solid fa-plus" /> */}
                 </div>
+                <Container className='p-0 d-flex flex-column h-100'>
+                    { props.tasks && 
+                    props.tasks.map((task: Task, index: number) => {
+                        return (
+                            <TaskComponent 
+                            title={task.title} 
+                            id={task.id}
+                            description={task.description}
+                            key={task.id}
+                            />
+                        )
+                    })
+                    }
+                </Container>
             </Col>
         </>
     )
--- pages/board.tsx
+++ pages/board.tsx
@@ -3,6 +3,10 @@ import { gql, useQuery } from '@apollo/client'
 import { Header } from '../components/layout/Header';
 import { Row } from 'react-bootstrap'
 import { TaskComponent } from '../components/taskComponent';
+import { BoardSection } from '../components/boardSection';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
 
 const AllTasksQuery = gql`
     query {
@@ -10,11 +14,12 @@ const AllTasksQuery = gql`
             id
             title
             description
+            status
         }
     }
 `
 
-const sections: Array<String> = ['Back-log', 'In Progress', 'Review', 'Done']
+const sections: Array<string> = ['Backlog', 'In Progress', 'Review', 'Done']
 
 const Board = () => {
     const { data, loading, error } = useQuery(AllTasksQuery, {
@@ -22,20 +27,22 @@ const Board = () => {
         console.log(data.tasks)
       }
     });
+
+    if (loading) return <p>Loading</p>
+    if (error) return <p>Error</p>
   return (<>
     <div className='d-flex flex-column h-100 pt-3'>
         <Row>
             <h1>project title</h1>
+            <FontAwesomeIcon icon={faPlus} style={{color: '#6f7782'}} />
         </Row>
         <div className="board-container d-flex flex-row flex-grow-1">
-            {data && data.tasks.map((task: Task) => {
-                return (
-                    <TaskComponent 
-                    title={task.title} 
-                    id={task.id}
-                    description={task.description}
-                    key={task.id}
-                    />
+            {sections.map((section: string, idx: number) => {
+                let filteredData: Array<Task> = data ? data.tasks.filter((task:Task) => {return task.status===section}) : [];
+                return(
+                    <>
+                    <BoardSection key={idx} title={section} tasks={filteredData}/>
+                    </>
                 )
             })}
         </div>
