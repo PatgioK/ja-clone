bd169d004b8d350a354db1f2183a2a2e4172593c
pat <patgiok@gmail.com>
2022-09-26 20:21:26 -0700

    can't get data CORS blocked from apollo.ts

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
 
-function board() {
+const AllTasksQuery = gql`
+    query {
+        tasks{
+            id
+            title
+            description
+        }
+    }
+`
+
+
+const Board = () => {
+    const { data, loading, error } = useQuery(AllTasksQuery, {
+      onCompleted: data => {
+        console.log(data.tasks)
+      }
+    });
   return (
     <div>board</div>
   )
 }
 
-export default board
+export default Board;
+
+
