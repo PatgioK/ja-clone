5da26d22db666bb7d5404e20f68ac53b09f9a4ac
pat <patgiok@gmail.com>
2022-09-28 15:31:25 -0700

    login page only works on chrome, add to schema for emailprovider  save before change from sendgrid to mailtrap

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,31 +1,31 @@
-// https://next-auth.js.org/adapters/prisma
-
-import NextAuth from "next-auth"
-import EmailProvider from 'next-auth/providers/email'
-import { NextApiHandler } from "next"
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+// import prisma from "../../../lib/prisma";
 
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
 const options = {
-    providers: [
-        EmailProvider({
-            // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-            server: {
-                host: process.env.SMTP_HOST,
-                port: Number(process.env.SMTP_PORT),
-                auth: {
-                    user: process.env.SMTP_USER,
-                    pass: process.env.SMTP_PASSWORD,
-                }
-            },
-            from: process.env.SMTP_FROM,
-        }),
-    ],
-    adapter: PrismaAdapter(prisma),
-    secret: process.env.SECRET,
-}
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.SMTP_FROM,
+    }),
+  ],
+  // @ts-ignore
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};
