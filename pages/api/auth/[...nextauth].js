b539c21a28e4c1b37b1da99cbf1302b184039050
pat <patgiok@gmail.com>
2022-09-27 18:58:12 -0700

    added next-auth stuff. commit b4 npm install next-auth @next-auth/prisma-adapter

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+// https://next-auth.js.org/adapters/prisma
+
+import NextAuth from "next-auth"
+import EmailProvider from `next-auth/providers/email`
+import { NextApiHandler } from "next"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+    providers: [
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM,
+            // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+
+            server: {
+                host: process.env.SMTP_HOST,
+                port: Number(process.env.SMTP_PORT),
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD,
+                }
+            }
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.SECRET,
+}
+
+
+// export default NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+// })
