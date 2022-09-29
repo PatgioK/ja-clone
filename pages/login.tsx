d462a851cb0ebebf870348df2468d5cf247b7c2c
pat <patgiok@gmail.com>
2022-09-28 20:28:40 -0700

    login redirect to board, npx prisma generate

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,7 +9,7 @@ const Login = () => {
 
     useEffect(() => {
         if(session) {
-            // router.push('/board')
+            router.push('/board')
             // signOut();
         }
     }, [session])
